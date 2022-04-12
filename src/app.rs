5557660f7500bd6a819d435d53d9f34c495708b2
Bruno A. Muciño <mucinoab@gmail.com>
2022-04-12 00:35:33 -0500

    Add visual aid (emoji) for stickers

--- src/app.rs
+++ src/app.rs
@@ -17,7 +17,7 @@ use presage::prelude::proto::{AttachmentPointer, ReceiptMessage, TypingMessage};
 use presage::prelude::{
     content::{ContentBody, DataMessage, Metadata, SyncMessage},
     proto::{
-        data_message::{Quote, Reaction},
+        data_message::{Quote, Reaction, Sticker},
         sync_message::Sent,
         GroupContextV2,
     },
@@ -799,8 +799,9 @@ impl App {
                             timestamp: Some(timestamp),
                             message:
                                 Some(DataMessage {
-                                    body,
+                                    mut body,
                                     attachments: attachment_pointers,
+                                    sticker,
                                     ..
                                 }),
                             ..
@@ -810,6 +811,8 @@ impl App {
             ) if destination_uuid.parse() == Ok(user_id) => {
                 let channel_idx = self.ensure_own_channel_exists();
                 let attachments = self.save_attachments(attachment_pointers).await;
+                add_emoji_from_sticker(&mut body, sticker);
+
                 let message = Message::new(user_id, body, timestamp, attachments);
                 (channel_idx, message)
             }
@@ -831,10 +834,11 @@ impl App {
                             timestamp: Some(timestamp),
                             message:
                                 Some(DataMessage {
-                                    body,
+                                    mut body,
                                     group_v2,
                                     quote,
                                     attachments: attachment_pointers,
+                                    sticker,
                                     ..
                                 }),
                             ..
@@ -867,6 +871,7 @@ impl App {
                     return Ok(());
                 };
 
+                add_emoji_from_sticker(&mut body, sticker);
                 let quote = quote.and_then(Message::from_quote).map(Box::new);
                 let attachments = self.save_attachments(attachment_pointers).await;
                 let message = Message {
@@ -888,12 +893,13 @@ impl App {
                     ..
                 },
                 ContentBody::DataMessage(DataMessage {
-                    body,
+                    mut body,
                     group_v2,
                     timestamp: Some(timestamp),
                     profile_key: Some(profile_key),
                     quote,
                     attachments: attachment_pointers,
+                    sticker,
                     ..
                 }),
             ) => {
@@ -930,6 +936,8 @@ impl App {
 
                     (channel_idx, from)
                 };
+
+                add_emoji_from_sticker(&mut body, sticker);
 
                 let attachments = self.save_attachments(attachment_pointers).await;
                 self.notify_about_message(&from, body.as_deref(), &attachments);
@@ -1613,6 +1621,12 @@ fn notification_text_for_attachments(attachments: &[Attachment]) -> Option<Strin
         0 => None,
         1 => Some("<attachment>".into()),
         n => Some(format!("<attachments ({n})>")),
+    }
+}
+
+fn add_emoji_from_sticker(body: &mut Option<String>, sticker: Option<Sticker>) {
+    if let Some(Sticker { emoji: Some(e), .. }) = sticker {
+        *body = Some(format!("<{}>", e));
     }
 }
 
