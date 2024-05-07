883c55949bbb11856998a0741e620f44f62a7774
boxdot <d@zerovolt.org>
2024-05-07 17:49:21 +0200

    clippy

--- src/app.rs
+++ src/app.rs
@@ -986,7 +986,7 @@ impl App {
                 message.reactions.swap_remove(idx);
                 false
             } else {
-                message.reactions[idx].1 = emoji.clone();
+                message.reactions[idx].1.clone_from(&emoji);
                 true
             }
         } else {
