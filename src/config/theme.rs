9e55e792b458c6aec8a0e6984c82b28f8aba8395
John Toohey <john_t@mailo.com>
2026-02-27 17:24:21 +0000

    Create an InputConfig for input boxes

--- src/config/theme.rs
+++ src/config/theme.rs
@@ -1,8 +1,8 @@
 use ratatui::{
     layout::HorizontalAlignment,
     style::Style,
-    text::Line,
-    widgets::{Block, BorderType, List, ListItem, Padding},
+    text::{Line, Text},
+    widgets::{Block, BorderType, List, ListItem, Padding, Paragraph},
 };
 use serde::{Deserialize, Serialize};
 
@@ -12,8 +12,8 @@ pub struct ThemeConfig {
     pub channel_popup: ChannelPopupConfig,
     #[serde(default = "ListThemeConfig::default_channel_list")]
     pub channels: ListThemeConfig,
-    #[serde(default = "BlockConfig::default_input")]
-    pub input: BlockConfig,
+    #[serde(default)]
+    pub input: InputConfig,
     #[serde(default)]
     pub messages: MessagesThemeConfig,
 }
@@ -23,7 +23,7 @@ impl Default for ThemeConfig {
         Self {
             channel_popup: ChannelPopupConfig::default(),
             channels: ListThemeConfig::default_channel_list(),
-            input: BlockConfig::default_input(),
+            input: InputConfig::default(),
             messages: MessagesThemeConfig::default(),
         }
     }
@@ -31,14 +31,16 @@ impl Default for ThemeConfig {
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct ChannelPopupConfig {
-    pub input: BlockConfig,
+    #[serde(default = "InputConfig::default_channel_popup")]
+    pub input: InputConfig,
+    #[serde(default)]
     pub list: ListThemeConfig,
 }
 
 impl Default for ChannelPopupConfig {
     fn default() -> ChannelPopupConfig {
         ChannelPopupConfig {
-            input: BlockConfig::default_channel_popup(),
+            input: InputConfig::default_channel_popup(),
             list: ListThemeConfig::default_style_unamed(),
         }
     }
@@ -98,8 +100,12 @@ impl ThemedText {
         Self::new(text, Style::new())
     }
 
-    pub fn widget<'a>(&'a self) -> Line<'a> {
+    pub fn line<'a>(&'a self) -> Line<'a> {
         Line::from(self.text.as_str()).style(self.style)
+    }
+
+    pub fn text<'a>(&'a self) -> Text<'a> {
+        Text::from(self.text.as_str()).style(self.style)
     }
 }
 
@@ -205,14 +211,6 @@ impl BlockConfig {
         }
     }
 
-    fn default_channel_popup() -> Self {
-        Self::unstyled("Select channel")
-    }
-
-    fn default_input() -> BlockConfig {
-        Self::unstyled("Input")
-    }
-
     fn default_messages() -> BlockConfig {
         Self::unstyled("Messages")
     }
@@ -249,7 +247,7 @@ impl BlockTitleConfig {
     }
 
     pub fn widget<'a>(&'a self) -> Line<'a> {
-        self.themed_text.widget().alignment(self.alignment)
+        self.themed_text.line().alignment(self.alignment)
     }
 }
 
@@ -263,4 +261,39 @@ fn default_user_styles() -> Vec<Style> {
         Style::new().cyan(),
         Style::reset(),
     ]
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct InputConfig {
+    pub block: BlockConfig,
+    pub text: Style,
+}
+
+impl InputConfig {
+    pub fn unstyled<S>(title: S) -> Self
+    where
+        String: From<S>,
+    {
+        Self {
+            block: BlockConfig::unstyled(title),
+            text: Style::new(),
+        }
+    }
+
+    fn default_channel_popup() -> InputConfig {
+        Self::unstyled("Select channel")
+    }
+
+    pub fn widget<'a, S>(&'a self, content: S) -> Paragraph<'a>
+    where
+        Text<'a>: From<S>,
+    {
+        Paragraph::new(Text::from(content).style(self.text)).block(self.block.widget())
+    }
+}
+
+impl Default for InputConfig {
+    fn default() -> Self {
+        Self::unstyled("Input")
+    }
 }
